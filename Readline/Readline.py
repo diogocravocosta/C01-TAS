a0ecd2bc1cbe701b902a37f9c8d481539b6c4c13
UnloadSomeCode <unveilmymail@protonmail.com>
2024-03-12 12:49:56 +0100

    updated readline function to exclude lines outside specified sample

--- Readline/Readline.py
+++ Readline/Readline.py
@@ -13,15 +13,22 @@ for i in range(0,len(content)):
         newsample = newsample + "N" + str(currentsamples) + "L" + str(i)
         currentsamples += 1
 
+#newsample is a string of format N[sample number]L[first dataline of sample]
+#looks like: N0L1N1L202N2L403
 
 def xyvalues(sample, line):
-    relevantstring = newsample[newsample.find("N"+str(sample)):newsample.find("N"+str(sample+1))]
+    relevantstring = newsample[newsample.find("N"+str(sample)):newsample.find("N"+str(sample+1))] 
     samplestart = relevantstring[ relevantstring.find("L")+1:]
-    samplestart
-    line = line + int(samplestart) - 1
-    lineStr = content[line]
-    xval = float(lineStr[3:17])
-    yval = float(lineStr[18:33])
-    return xval, yval
+    
+    next_relevantstring = newsample[newsample.find("N"+str(sample+1)):newsample.find("N"+str(sample+2))]
+    next_samplestart = next_relevantstring[next_relevantstring.find("L")+1:]
 
-#print(xyvalues(2,2))
+    line = line + int(samplestart) -1
+    if line > int(next_samplestart) - 3 or line - int(samplestart) < 0 : #returns False if a line outside the specified sample is summoned
+        return False
+    else:
+
+        lineStr = content[line]
+        xval = float(lineStr[3:17])
+        yval = float(lineStr[18:33])
+        return xval, yval
