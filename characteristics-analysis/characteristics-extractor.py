7b9c81730bce7f053df604a6566cf3480485552a
diogocravocosta <diogocravocosta@gmail.com>
2024-03-14 15:38:58 +0100

    new changes for thickness to chord

--- characteristics-analysis/characteristics-extractor.py
+++ characteristics-analysis/characteristics-extractor.py
@@ -1,2 +1,8 @@
 #program to extract certain parameters of an airfoil, as described in Chacteristics-list.md
 
+from output_data_cleaner import get_airfoil_data
+
+
+def thicknes_to_chord(airfoil): #returns the thickness to chord from a certain airfoil
+
+    return
