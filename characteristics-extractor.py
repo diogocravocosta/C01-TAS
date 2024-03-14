8ff2c81d9b91d2e7f7966c1859cec9a01bafc146
diogocravocosta <diogocravocosta@gmail.com>
2024-03-14 15:51:47 +0100

    Reorganized file structure

--- characteristics-extractor.py
+++ characteristics-extractor.py
@@ -2,7 +2,8 @@
 
 from output_data_cleaner import get_airfoil_data
 
-
 def thickness_to_chord(sample_num): #returns the thickness to chord from a certain airfoil, sample_num is the sample number of the airfoil
-    get_airfoil_data(one_value= True, sample_num = 1)
-    return
+    samples, latent_parameters = get_airfoil_data(one_value= True, sample_num = 1)
+    return samples, latent_parameters
+
+print(thickness_to_chord(1))
