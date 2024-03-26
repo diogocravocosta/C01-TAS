9e57d4aa14879eaa81bbcb7d4ba0ba4dfb6af57f
astroash04 <ashwanth2004@gmail.com>
2024-03-26 11:22:22 +0100

    update

--- regression.py
+++ regression.py
@@ -1,7 +1,6 @@
 #import characteristics_determination as cd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
 fig = plt.figure()
 ax = fig.add_subplot()
 def quadratic(x ,a, b, c):
@@ -9,7 +8,7 @@ def quadratic(x ,a, b, c):
     return y
 
 def regression(x,y,degree):
-    if degree in ['linear', "Linear", 'lineaire', 'LINEAR', 'John Cena', 1, 'one']:
+    if degree in ['linear', 'Linear', 'lineaire', 'LINEAR', 'John Cena', 1, 'one']:
         #Values = np.array([x,y])
         parameters = np.polyfit(x,y,1,full=True)
         response = []
@@ -25,21 +24,34 @@ def regression(x,y,degree):
         response.append(Result[1])
         response.append(Result[2])
         response.append(Residuals[0])
+    #elif degree in ['Cubic', 'cubic', 3, 'three']:
+        #Value = np.polyfit(x,y,3, full = True)
+        #Results = Value[0]
+        #Residual = Value[1]
+        #response = []
+        #response.append(Results[0])
+        #response.append(Results[1])
+        #response.append(Results[2])
+        #response.append(Results[3])
+        #response.append(Residual[0])
+        #print(Value)
 
 
 
     return response
 
 x = [1,2,3,4]
-y = [4,8,12,16]
-regress = regression(x,y,1)
+y = [1,8,27,100]
+regress = regression(x,y,3)
 print(regress)
 ax.scatter(x,y)
 x_array = np.arange(x[0],x[-1], 0.0001)
+y_array = regress[0] * x_array**3 + regress[1] * x_array**2 + regress[2] * x_array + regress[3]
+#ax.text(x[0],y[-1], 'Regression: y = ' + str(round(regress[0], 4)) +'$x^3$ +' + str(round(regress[1], 4)) + '$x^2$ +' + str(round(regress[2], 4)) +  ' | R^2 = ' + str(round(regress[3], 4)) , fontsize = 10, color = 'red')
 #y_array = regress[0] * x_array**2 + regress[1] * x_array + regress[2]
 #ax.text(x[0],y[-1], 'Regression: y = ' + str(round(regress[0], 4)) +'x**2 +' + str(round(regress[1], 4)) + 'x +' + str(round(regress[2], 4)) +  ' | R^2 = ' + str(round(regress[3], 4)) , fontsize = 10, color = 'red')
-y_array = regress[0] * x_array + regress[1]
-ax.text(x[0], y[-1], 'Regression: y = ' + str(round(regress[0], 4)) + 'x + ' + str(round(regress[1], 4)) + ' | R^2 = ' + str(round(regress[2][0], 4)))
+#y_array = regress[0] * x_array + regress[1]
+#ax.text(x[0], y[-1], 'Regression: y = ' + str(round(regress[0], 4)) + 'x + ' + str(round(regress[1], 4)) + ' | R^2 = ' + str(round(regress[2][0], 4)))
 ax.plot(x_array, y_array, 'purple')
 
 plt.show()
