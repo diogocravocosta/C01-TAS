ae86ec6bb7fc8d642a7ede6d9c8d813f38486283
Bearking101 <grigas.ioannis@gmail.com>
2024-03-26 10:53:22 +0100

    :) hehe nice

--- PCA code.py
+++ PCA code.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 
-parameters_list = X = []
+X = []
 
 def by_sklearn(X:np.array, k:int):
 
@@ -28,10 +28,10 @@ data_skl, var_skl = by_sklearn(data, n_red)
 fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 fig.suptitle(f"{n}-D data as {n_red}-D data")
 
-axs[1].scatter(data_skl[:,0],data_skl[:,1],color="r",s=1)
-axs[1].set_title("Sklearn")
-axs[1].set_xlabel(f"Variance: {round(var_skl[0],4)}")
-axs[1].set_ylabel(f"Variance: {round(var_skl[1],4)}")
+axs[0].scatter(data_skl[:,0],data_skl[:,1],color="r",s=1)
+axs[0].set_title("Sklearn")
+axs[0].set_xlabel(f"Variance: {round(var_skl[0],4)}")
+axs[0].set_ylabel(f"Variance: {round(var_skl[1],4)}")
 
 fig.tight_layout()
 plt.show()
