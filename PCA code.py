9a4ce3df91ef889d4de96845dbe811bb71761622
Nabo2212 <nabihah@zaheer.com>
2024-03-26 11:33:32 +0100

    poop

--- PCA code.py
+++ PCA code.py
@@ -7,10 +7,11 @@ from output_data_cleaner import get_output_airfoil_data
 
 _, unsorted_lps = get_output_airfoil_data(False, 0)
 
-def n_parameters_list (n1, n2): # n1 is the first latent parameter you wanna look at, n2 is the second one you wanna compare
-    return [[data_point[n1], data_point[n2]] for data_point in unsorted_lps][1:]
+def n_parameters_list (n1, n2, n3, n4, n5): # n1 is the first latent parameter you wanna look at, n2 is the second one you wanna compare
+    return [[data_point[n1], data_point[n2], data_point[n3], data_point[n4], data_point[n5]] for data_point in unsorted_lps][1:]
 
-n_parameters_list = X = np.array(n_parameters_list(1, 2))
+X = np.array(n_parameters_list(0,1,2,3,4))
+X2 = np.array(n_parameters_list(3,4,5,6,7))
 
 def by_sklearn(X:np.array, k:int):
 
@@ -28,17 +29,29 @@ def by_sklearn(X:np.array, k:int):
 
     return new_data, variances
 
-# perform PCA on data using both PCA from scratch and PCA from sklearn
-data_skl, var_skl = by_sklearn(data, n_red)
+# perform PCA on data using PCA from sklearn
+data_skl, var_skl = by_sklearn(X, 2)
+data_skl2, var_skl2 = by_sklearn(X2, 2)
+
 
 # plot
 fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-fig.suptitle(f"{n}-D data as {n_red}-D data")
+# fig.suptitle(f"{n}-D data as {n_red}-D data")
+
+# x_values, y_values = zip(*n_parameters_list(0,1))
+# axs[0].scatter(x_values, y_values, color='b', s=0.5)
+# x_values2, y_values2 = zip(*n_parameters_list(0,2))
+# axs[1].scatter(x_values2, y_values2, color='b', s=0.5)
 
 axs[0].scatter(data_skl[:,0],data_skl[:,1],color="r",s=1)
 axs[0].set_title("Sklearn")
 axs[0].set_xlabel(f"Variance: {round(var_skl[0],4)}")
 axs[0].set_ylabel(f"Variance: {round(var_skl[1],4)}")
+
+axs[1].scatter(data_skl2[:,0],data_skl2[:,1],color="green",s=1)
+axs[1].set_title("Sklearn")
+axs[1].set_xlabel(f"Variance: {round(var_skl2[0],4)}")
+axs[1].set_ylabel(f"Variance: {round(var_skl2[1],4)}")
 
 fig.tight_layout()
 plt.show()
