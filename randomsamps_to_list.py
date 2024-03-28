3685977c2dc1cc42e1851206567482957ae36f20
diogocravocosta <diogocravocosta@gmail.com>
2024-03-28 15:28:21 +0100

    randomsamps_to_list finished

--- randomsamps_to_list.py
+++ randomsamps_to_list.py
@@ -11,3 +11,25 @@ with open('random_samples.txt', "r") as file:
         if line.startswith('Sample'):
             #Start of a new Sample
             sample_count += 1 
+        elif line.startswith('latent parameters:'):
+            for i in range(8):
+                latent_param = float(next(file).strip())
+                latent_parameters_lists[i].append(latent_param)
+        elif line.startswith('maxcamb_position'):
+            characteristics_lists[0].append(float(line.split(":")[1].strip()))
+        elif line.startswith('maxcamb'):
+            characteristics_lists[1].append(float(line.split(":")[1].strip()))
+        elif line.startswith('LE_angle'):
+            characteristics_lists[2].append(float(line.split(":")[1].strip()))
+        elif line.startswith('TE_angle'):
+            characteristics_lists[3].append(float(line.split(":")[1].strip()))
+        elif line.startswith('thickness_to_chord'):
+            characteristics_lists[4].append(float(line.split(":")[1].strip()))
+        elif line.startswith('max_thickness_position'):
+            characteristics_lists[5].append(float(line.split(":")[1].strip()))
+
+#Print/use lists as needed
+
+#print('Latent Parameters:')
+#for i, latent_list in enumerate(latent_parameters_lists):
+#    print(f"Latent PA")
