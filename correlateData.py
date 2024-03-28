7250f4761d5a5cba3b25be09f7a4140b64596ca0
UnloadSomeCode <unveilmymail@protonmail.com>
2024-03-28 14:11:09 +0100

    correlateData function made

--- correlateData.py
+++ correlateData.py
@@ -8,53 +8,109 @@ from input_data_cleaner import get_input_data
 from numpy.polynomial import Polynomial
 import warnings
 
+from thickness_characteristics_extractor import random_thickness_to_chord, input_thickness_to_chord
 
-file1 = open("data.txt", "a")
+#xlist = [] #parameters
+#ylist = [] #characteristic
+
+#def correlateData(characteristic, Parameter, File):
+#    filelen = 10000
+#    if File == 0: #"output"
+#        return 1
+#    elif File == 1: #"input"
+#        _,_,_,A = get_input_data()
+#        filelen = len(A)
+#        for i in range(min, max): #range(len(A)):
+#            xlist.append(A[i][Parameter])
+#    else:
+#        return False
+#
+#    if characteristic < 6:
+#        for i in range(min, max):
+#            #r = 5
+#            B = camber(i,File)
+#            ylist.append(B[characteristic])
+#
+#
+#    for i in range(0,len(ylist)):
+#        file1.write(str(xlist[i])+","+str(ylist[i])+"\n")
+#
+#    print("done")
+#    #return 8 #xlist[1],ylist[1]
 
 
-xlist = [] #parameters
-ylist = [] #characteristic
+
+#def correlateData():
+    #filelen = 10000
+    #if File == 0: #"output"
+    #    return 1
+    #elif File == 1: #"input"
+    #    _,_,_,A = get_input_data()
+    #    filelen = len(A)
+    #    for i in range(min, max): #range(len(A)):
+    #        xlist.append(A[i][Parameter])
+    #else:
+    #    return False
+
+    #if characteristic < 6:
+    #    for i in range(min, max):
+    #        #r = 5
+    #        B = camber(i,File)
+    #        ylist.append(B[characteristic])
 
 
-min = 400
-max = min + 200
 
-def correlateData(characteristic, Parameter, File):
-    filelen = 10000
-    if File == 0: #"output"
-        return 1
-    elif File == 1: #"input"
+    #for i in range(0,len(ylist)):
+    #    file1.write(str(xlist[i])+","+str(ylist[i])+"\n")
+
+
+
+file1 = open("Regressdata.txt", "a")
+
+min = 0
+max = min + 20
+
+def correlateData():
+
+    for num in range(min, max):
         _,_,_,A = get_input_data()
-        filelen = len(A)
-        for i in range(min, max): #range(len(A)):
-            xlist.append(A[i][Parameter])
-    else:
-        return False
+        B,C,D,E,F = camber(num,1)
+        G,H = input_thickness_to_chord(num)
 
-    if characteristic < 6:
-        for i in range(min, max):
-            #r = 5
-            B = camber(i,File)
-            ylist.append(B[characteristic])
+        line = str(num) + A[num] + "(" + C + "," + D + "," + E + "," + F + "," + G + "," + H + ")"
 
-
-    for i in range(0,len(ylist)):
-        file1.write(str(xlist[i])+","+str(ylist[i])+"\n")
+        file1.write(line+"\n")
 
     print("done")
-    #return 8 #xlist[1],ylist[1]
 
 
+
+# Characteristic index
+    
+# 0 - x_maxcamb
+# 1 - maxcamb
+# 2 - LE_angle
+# 3 - TE_angle
+# 4 - thickness_to_chord
+# 5 - thickness_pos
+
+
+
+correlateData()
 
 
 file1.close()
 
 
-finalDat = []
-def dataToArray():
-    file2 = open("data.txt", "r")
-    content = file2.readlines()
-    for i in range(len(content)):
-        finalDat.append(content[i][:content[i].find(",")] , content[i][content[i].find(",")+1:])
-    return finalDat
-    file2.close()
+
+
+
+
+#finalDat = []
+#def dataToArray():
+#    file2 = open("Regressdata.txt", "r")
+#    content = file2.readlines()
+#    for i in range(len(content)):
+#        finalDat.append(content[i][:content[i].find(",")] , content[i][content[i].find(",")+1:])
+#    return finalDat
+#    file2.close()
