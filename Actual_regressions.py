f22bb1107488168b2ce231352b179c2a1230f7e0
UnloadSomeCode <unveilmymail@protonmail.com>
2024-04-23 10:07:56 +0200

    symmetric attempts

--- Actual_regressions.py
+++ Actual_regressions.py
@@ -4,11 +4,22 @@ from plot_generator import generate
 import csv
 from regression import regression
 latent_parameters_lists, characteristics_lists = samples_to_list('Regressdata.txt')
+latent_parameters_lists_symmetric, characteristics_lists_symmetric = samples_to_list_symmetric('Regressdata.txt')
+
 r_csv_values=[]
 for param in range(8):
     r_param_values = []
-    for char in range(6): 
-        generate(latent_parameters_lists[param],characteristics_lists[char],"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter "+ str(param), "Characteristic "+ str(char))
+    for char in range(6):
+        parlist = []
+        charlist = []
+        if char <= 1:
+            parlist = latent_parameters_lists_symmetric[param]
+            charlist = characteristics_lists_symmetric[char]
+        else:
+            parlist = latent_parameters_lists[param]
+            charlist = characteristics_lists[char]
+
+        generate(parlist , charlist,"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter "+ str(param), "Characteristic "+ str(char))
         r_vals = []
         for i in range(3):
             r_vals.append(regression(latent_parameters_lists[param],characteristics_lists[char], i+1)[-1])
@@ -24,3 +35,37 @@ with open(csv_file_path, mode='w', newline='') as file:
     # Write data to the CSV file
     writer.writerows(r_csv_values) #x is latent parameters, Y is characteristics
 
+
+
+
+
+
+
+
+
+
+
+
+#   latent_parameters_lists, characteristics_lists = samples_to_list('Regressdata.txt')
+#   
+#   r_csv_values=[]
+#   for param in range(8):
+#       r_param_values = []
+#       for char in range(6):
+#           generate(latent_parameters_lists[param],characteristics_lists[char],"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter "+ str(param), "Characteristic "+ str(char))
+#           r_vals = []
+#           for i in range(3):
+#               r_vals.append(regression(latent_parameters_lists[param],characteristics_lists[char], i+1)[-1])
+#           nice_r = min(r_vals)
+#           r_param_values.append(nice_r)
+#       r_csv_values.append(r_param_values)
+#   
+#   csv_file_path = 'r_values_outputs.csv'
+#   
+#   with open(csv_file_path, mode='w', newline='') as file:
+#       # Create a csv.writer object
+#       writer = csv.writer(file)
+#       # Write data to the CSV file
+#       writer.writerows(r_csv_values) #x is latent parameters, Y is characteristics
+#   
+#   
--- randomsamps_to_list.py
+++ randomsamps_to_list.py
@@ -1,13 +1,13 @@
-def samples_to_list(path, char_val):
+def samples_to_list(path):
     # Initialize lists for latent parameters and characteristics 
     latent_parameters_lists = [[], [], [], [], [], [], [], []]
     characteristics_lists = [[], [], [], [], [], []]
-    skip = 0
+    #skip = 0
     # Open the file
     with open(path, 'r') as file:
         sample_count = -1
         for line in file:
-            skip = True
+            #skip = True
             line = line.strip()
             if line.startswith(('maxcamb_position', 'maxcamb', 'LE_angle', 'TE_angle', 'thickness_to_chord', 'max_thickness_position')):
                 characteristic_name, characteristic_value = line.split(":")
@@ -17,25 +17,25 @@ def samples_to_list(path, char_val):
                 # Determine the index of the characteristic within characteristics_lists
                 if characteristic_name == 'maxcamb_position':
                     char_index = 0
-                    if float(characteristic_value) > -1.01 and float(characteristic_value) < -0.99 and char_val == 0:
-                        skip = True
-                        continue
-                elif characteristic_name == 'maxcamb'and char_val == 1:
-                    if float(characteristic_value) > -1.01 and float(characteristic_value) < -0.99:
-                        characteristic_value = 0
+                    #if float(characteristic_value) > -1.01 and float(characteristic_value) < -0.99 and char_val == 0:
+                        #skip = True
+                        #continue
+                elif characteristic_name == 'maxcamb': #and char_val == 1:
+                    #if float(characteristic_value) > -1.01 and float(characteristic_value) < -0.99:
+                        #characteristic_value = 0
                     char_index = 1
-                elif characteristic_name == 'LE_angle' and char_val == 2:
+                elif characteristic_name == 'LE_angle': # and char_val == 2:
                     char_index = 2
-                elif characteristic_name == 'TE_angle'and char_val == 3:
+                elif characteristic_name == 'TE_angle': #and char_val == 3:
                     char_index = 3
-                elif characteristic_name == 'thickness_to_chord'and char_val == 4:
+                elif characteristic_name == 'thickness_to_chord': #and char_val == 4:
                     char_index = 4
-                elif characteristic_name == 'max_thickness_position'and char_val == 5:
+                elif characteristic_name == 'max_thickness_position': #and char_val == 5:
                     char_index = 5
                 characteristics_lists[char_index].append(float(characteristic_value))
             elif line.startswith('Sample'):
                 sample_count += 1
-            elif line.startswith('latent parameters:') and skip == False:
+            elif line.startswith('latent parameters:'): # and skip == False:
                 for i in range(8):
                     latent_param = float(next(file).strip())
                     latent_parameters_lists[i].append(latent_param)
@@ -47,8 +47,8 @@ def samples_to_list(path, char_val):
 
 def samples_to_list_symmetric(path):
     # Initialize lists for latent parameters and characteristics 
-    latent_parameters_lists = [[[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []]]
-    characteristics_lists = [ [[], []], [[], [], [], []] ]
+    latent_parameters_lists = [[[], [], [], [], [], [], [], []]]
+    characteristics_lists = [ [[], []] ]
 
     # Open the file
     with open(path, 'r') as file:
@@ -60,8 +60,7 @@ def samples_to_list_symmetric(path):
             elif line.startswith('latent parameters:'):
                 for i in range(8):
                     latent_param = float(next(file).strip())
-                    latent_parameters_lists[0][i].append(latent_param)
-                    latent_parameters_lists[1][i].append(latent_param)
+                    latent_parameters_lists[i].append(latent_param)
             elif line.startswith(('maxcamb_position', 'maxcamb', 'LE_angle', 'TE_angle', 'thickness_to_chord', 'max_thickness_position')):
                 characteristic_name, characteristic_value = line.split(":")
                 characteristic_value = characteristic_value.strip()
@@ -82,22 +81,22 @@ def samples_to_list_symmetric(path):
                     char_index = 5
 
                 if char_index == 0 and characteristic_value != -1:
-                    characteristics_lists[0][char_index].append(float(characteristic_value))
+                    characteristics_lists[char_index].append(float(characteristic_value))
+
                 elif char_index == 1:
                     if characteristic_value != -1:
-                        characteristics_lists[0][char_index].append(float(characteristic_value))
+                        characteristics_lists[char_index].append(float(characteristic_value))
                     else:
                         for i in range(8):
-                            latent_parameters_lists[0][i].pop                      
-                else:
-                    characteristics_lists[1][char_index].append(float(characteristic_value))
+                            latent_parameters_lists[i].pop                      
+                
+                #characteristics_lists[1][char_index].append(float(characteristic_value))
 
                 #if char_index == 1 and characteristic_value == -1:
                 #    characteristics_lists[0].pop
                 #    characteristics_lists[1].pop
                 #    for i in range(8):
                 #        latent_parameters_lists[i].pop
-
 
     # Return the lists
     return latent_parameters_lists, characteristics_lists
