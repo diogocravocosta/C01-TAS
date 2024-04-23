5cae427c9975d62c946a3590c386202540660a22
diogocravocosta <diogocravocosta@gmail.com>
2024-04-23 09:08:42 +0200

    Additional function regression edit

--- Actual_regressions.py
+++ Actual_regressions.py
@@ -6,5 +6,5 @@ from plot_generator import generate
 latent_parameters_lists, characteristics_lists = samples_to_list('Regressdata.txt')
 for param in range(8):
     for char in range(6): 
-        generate(latent_parameters_lists[param],characteristics_lists[char],"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter", "Characteristic "+ str(char))
+        generate(latent_parameters_lists[param],characteristics_lists[char],"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter "+ str(param), "Characteristic "+ str(char))
 
