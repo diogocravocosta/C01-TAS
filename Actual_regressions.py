d959fc24e2adc4b588f9d2e917d30c7ca5134500
diogocravocosta <diogocravocosta@gmail.com>
2024-04-23 11:49:49 +0200

    New regression plots and csv

--- Actual_regressions.py
+++ Actual_regressions.py
@@ -2,6 +2,7 @@
 from randomsamps_to_list import samples_to_list, samples_to_list_symmetric
 from plot_generator import generate
 import csv
+import numpy as np
 from regression import regression
 latent_parameters_lists, characteristics_lists = samples_to_list('Regressdata.txt')
 latent_parameters_lists_symmetric, characteristics_lists_symmetric = samples_to_list_symmetric('Regressdata.txt')
@@ -19,13 +20,43 @@ for param in range(8):
             parlist = latent_parameters_lists[param]
             charlist = characteristics_lists[char]
 
+        parlist = np.array(parlist)
+        charlist = np.array(charlist)
+        parlist = (parlist - np.mean(parlist))/ np.std(parlist)
+        charlist = (charlist - np.mean(charlist))/ np.std(charlist)
         generate(parlist , charlist,"Latent Parameter "+ str(param)+" and characteristic "+ str(char), "Latent Parameter "+ str(param), "Characteristic "+ str(char))
         r_vals = []
+        
         for i in range(3):
-            r_vals.append(regression(latent_parameters_lists[param],characteristics_lists[char], i+1)[-1])
+            r_vals.append(regression(parlist,charlist, i+1)[-1])
         nice_r = min(r_vals)
         r_param_values.append(nice_r)
     r_csv_values.append(r_param_values)
+
+#normalize r^2 (for some godforsaken reason)
+lst = [[],[],[],[],[],[]]
+def normalize(X):
+    X_normalized = (X - np.mean(X)) / np.std(X)
+    return X_normalized
+
+for i in range(8):
+    for c in range(6):
+        lst[c].append(r_csv_values[i][c]) 
+r_0 = list(normalize(np.array(lst[0])))
+r_1 = list(normalize(np.array(lst[1])))
+r_2 = list(normalize(np.array(lst[2])))
+r_3 = list(normalize(np.array(lst[3])))
+r_4 = list(normalize(np.array(lst[4])))
+r_5 = list(normalize(np.array(lst[5])))
+
+r_csv_values_mk2 = []
+
+r_csv_values_mk2.append(r_0)
+r_csv_values_mk2.append(r_1)
+r_csv_values_mk2.append(r_2)
+r_csv_values_mk2.append(r_3)
+r_csv_values_mk2.append(r_4)
+r_csv_values_mk2.append(r_5)
 
 csv_file_path = 'r_values_outputs.csv'
 
@@ -33,7 +64,7 @@ with open(csv_file_path, mode='w', newline='') as file:
     # Create a csv.writer object
     writer = csv.writer(file)
     # Write data to the CSV file
-    writer.writerows(r_csv_values) #x is latent parameters, Y is characteristics
+    writer.writerows(r_csv_values_mk2) #x is latent parameters, Y is characteristics
 
 
 
